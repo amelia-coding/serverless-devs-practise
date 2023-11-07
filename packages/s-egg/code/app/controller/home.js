32d1c75c3123811e4b0304b28251c16485d4c3b6
dujuan <dujuan@bixin.cn>
2023-11-07 21:35:33 +0800

    feat: syaml

--- packages/s-egg/code/app/controller/home.js
+++ packages/s-egg/code/app/controller/home.js
@@ -1,12 +1,17 @@
-'use strict';
+'use strict'
 
-const Controller = require('egg').Controller;
+const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
-    await ctx.render('home.nj', {});
+    const { ctx } = this
+    await ctx.render('home.nj', {})
+  }
+
+  async post() {
+    const { ctx } = this
+    ctx.body = { id: 1, name: '我是一段post请求数据' }
   }
 }
 
-module.exports = HomeController;
+module.exports = HomeController
