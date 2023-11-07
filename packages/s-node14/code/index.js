7359652eef08fe99614062a5a27957874ecd8958
dujuan <dujuan@bixin.cn>
2023-11-07 21:33:44 +0800

    feat: syaml

--- packages/s-node14/code/index.js
+++ packages/s-node14/code/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 /*
 To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
 please implement the initializer function as below：
@@ -8,11 +8,11 @@ exports.initializer = (context, callback) => {
 };
 */
 exports.fn1 = (event, context, callback) => {
-  console.log('hello fn1')
-  callback(null, 'hello fn1')
+    console.log('hello world fn1');
+    callback(null, 'hello world fn1');
 }
 
 exports.fn2 = (event, context, callback) => {
-  console.log('hello fn2')
-  callback(null, 'hello fn2')
+  console.log('hello world fn2');
+  callback(null, 'hello world fn2');
 }
